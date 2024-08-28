7bdc75c2faeca7677ac549599f73fb1a8fe4a10c
glock4101 <97336775+glock4101@users.noreply.github.com>
2024-08-28 22:35:17 +0200

    22
    22

--- components/smartbob_component.cpp
+++ components/smartbob_component.cpp
@@ -1,3 +1,50 @@
 #include "smartbob_component.h"
 
-// Implementacja metod, jeśli są potrzebne
+namespace esphome {
+  namespace smartbob {
+
+    void SmartBOBBinarySensor::setup() {
+      // Inicjalizacja, jeśli jest potrzebna
+    }
+
+    void SmartBOBBinarySensor::dump_config() {
+      ESP_LOGCONFIG(TAG, "SmartBOB Binary Sensor:");
+      ESP_LOGCONFIG(TAG, "  Sensor ID: %d", this->sensor_id_);
+    }
+
+    void SmartBOBComponent::setup() {
+      // Inicjalizacja UART lub inne operacje setup
+    }
+
+    void SmartBOBComponent::loop() {
+      if (this->uart_->available()) {
+        std::string data = this->uart_->read_str();
+        process_data(data);
+      }
+    }
+
+    void SmartBOBComponent::process_data(const std::string &data) {
+      int DevID = 0;
+      int SensorID = 0;
+      float SensorValue = 0;
+
+      if (sscanf(data.c_str(), "%d:%d:%f", &DevID, &SensorID, &SensorValue) == 3) {
+        publish_state(DevID, SensorID, SensorValue == 1);
+      }
+    }
+
+    void SmartBOBComponent::publish_state(int DevID, int SensorID, bool state) {
+      std::string sensor_id = "binary_sensor_" + std::to_string(DevID) + "_" + std::to_string(SensorID);
+      auto sensor = App.get_binary_sensor(sensor_id);
+      if (sensor) {
+        sensor->publish_state(state);
+      }
+    }
+
+    void SmartBOBComponent::dump_config() {
+      ESP_LOGCONFIG(TAG, "SmartBOB Component:");
+      // Dodatkowe informacje konfiguracyjne, jeśli są potrzebne
+    }
+
+  }
+}
--- components/smartbob_component.h
+++ components/smartbob_component.h
@@ -2,33 +2,44 @@
 
 #include "esphome.h"
 
-class SmartBOBComponent : public Component, public UARTDevice {
- public:
-  SmartBOBComponent(UARTComponent *parent) : UARTDevice(parent) {}
+#define TAG "smartbob.component"
 
-  void setup() override {
-    // Inicjalizacja, jeśli potrzebna
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/switch/switch.h"
+
+namespace esphome {
+  namespace smartbob {
+
+    class SmartBOBBinarySensor : public Component, public binary_sensor::BinarySensor {
+    public:
+      void set_target_relay_controller_addr(uint8_t addr) { this->target_relay_controller_addr_ = addr; }
+      uint8_t get_target_relay_controller_addr() { return this->target_relay_controller_addr_; }
+      void set_sensor_id(uint8_t sensor_id) { this->sensor_id_ = sensor_id; }
+      uint8_t get_sensor_id() { return this->sensor_id_; }
+      void setup() override;
+      void dump_config() override;
+
+    protected:
+      uint8_t target_relay_controller_addr_;
+      uint8_t sensor_id_;
+    };
+
+    class SmartBOBComponent : public Component, public uart::UARTDevice {
+    public:
+      SmartBOBComponent(uart::UARTComponent *uartComponent) : UARTDevice(uartComponent) {
+        this->uart_ = uartComponent;
+      }
+      void register_binary_sensor(smartbob::SmartBOBBinarySensor *obj) { this->binary_sensors_.push_back(obj); }
+      void setup() override;
+      void loop() override;
+      void dump_config() override;
+
+    protected:
+      uart::UARTComponent *uart_;
+      std::vector<smartbob::SmartBOBBinarySensor *> binary_sensors_;
+    };
+
   }
-
-  void loop() override {
-    if (available()) {
-      std::string data = read_str();
-      process_data(data);
-    }
-  }
-
-  void process_data(const std::string &data) {
-    int DevID = 0;
-    int SensorID = 0;
-    float SensorValue = 0;
-
-    if (sscanf(data.c_str(), "%d:%d:%f", &DevID, &SensorID, &SensorValue) == 3) {
-      publish_state(DevID, SensorID, SensorValue == 1);
-    }
-  }
-
-  void publish_state(int DevID, int SensorID, bool state) {
-    // Implementacja logiki publikacji stanu
-    // np. id(sensor_1_1).publish_state(state); itd.
-  }
-};
+}
